6193e65c21eedbcd9357b0667000e8325cba470e
Amine Koudri <akoudri@internal.salesforce.com>
2017-04-23 21:09:28 +0100

    before send to Azure

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
+//Add your requirements
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot
+({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
+bot.add('/', new builder.SimpleDialog( function (session) {
+session.send('Hello World');
+}));
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
+console.log('%s listening to %s', server.name, server.url);
+});
+
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
+/*
+
 // Add your requirements
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
+server.listen(process.env.PORT || 3978, function() 
 {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
+
+
+
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
+// Create bot and add dialogs
+//var bot = new builder.BotConnectorBot
+//({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
+
+
+
+
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
@@ -19,3 +60,12 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+
+
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
+
+
+*/
